ba15eaf95c3aed51efc21a2f6f52a004eb07f9da
Ronald Coello <ronaldcoello85@gmail.com>
2024-02-18 10:44:53 -0700

    added signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,27 +2,36 @@
 import { useEffect, useState } from "react";
 import { NextUIProvider, Input, Checkbox, Button } from "@nextui-org/react";
 import {getAllUsers, getUserByEmail, createUser, updateUserByEmail, deleteUserByEmail} from "../server_layer/user"
-import {createUserWithEmailPassword,signInUser} from "../server_layer/authentication"
+import {signup,signInUser} from "../server_layer/authentication"
 import {createAddress, getAllAddress, getAddressById, deleteAddressById, updateAddressById} from "../server_layer/address"
 import {createEventSite, getAllEventSite} from "../server_layer/eventSite"
-
+import toast from "react-hot-toast";
 
 export default function SignUp() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  // testing code : don't delete
-  useEffect(()=>{
-    const getAllUser = async()=>{
-      // const allusers = await createEventSite({"userId":"QyyVpciDsuc2XlQQ1n0A","email":"test1@gmail.netmail", "name":"test345dfsdfsdfs", "password":"123456rt"})
-      const allusers = await getAllEventSite("QyyVpciDsuc2XlQQ1n0A")
+  // // testing code : don't delete
+  // useEffect(()=>{
+  //   const getAllUser = async()=>{
+  //     // const allusers = await createEventSite({"userId":"QyyVpciDsuc2XlQQ1n0A","email":"test1@gmail.netmail", "name":"test345dfsdfsdfs", "password":"123456rt"})
+  //     const allusers = await getAllEventSite("QyyVpciDsuc2XlQQ1n0A")
 
-      console.log(allusers)
-    }
-    getAllUser()
+  //     console.log(allusers)
+  //   }
+  //   getAllUser()
     
-  }, [])
+  // }, [])
+  const createUser = async()=>{
+
+    const user = await signup({"email":email, "name":name, "password":password})
+    if (user){
+      toast.success("User created successfully")
+    }
+   
+    console.log(user)
+  }
   return (
     <NextUIProvider>
       <form className="mx-auto grid max-w-[600px] p-8">
@@ -53,6 +62,7 @@ export default function SignUp() {
             <button
               type="submit"
               className="bg-primary rounded-md py-3 text-white"
+              onClick={()=>{createUser()}}
             >
               Sign Up
             </button>
