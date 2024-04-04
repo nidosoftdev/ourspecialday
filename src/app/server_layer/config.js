415c72d279f042f7ce99b4e5ac2c70d5f7f50638
Ronald Coello <ronaldcoello85@gmail.com>
2024-04-03 18:31:13 -0600

    removed console

--- src/app/server_layer/config.js
+++ src/app/server_layer/config.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 import {getFirestore} from 'firebase/firestore'
-console.log(process.env.NEXT_PUBLIC_API_KEY)
+
 
 const firebaseConfig = {
   
