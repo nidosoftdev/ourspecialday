fddfc55ec8bda98356d5b640e2e24332d4105eb2
Lakeram Narine <nar19002@byui.edu>
2024-04-24 11:41:01 -0600

    testing

--- src/app/server_layer/config.js
+++ src/app/server_layer/config.js
@@ -16,6 +16,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+console.log(firebaseConfig)
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
 export const googleProvider = new GoogleAuthProvider()
