2854e86cb213d440c0029f3e102e477216c13705
Ronald Coello <ronaldcoello85@gmail.com>
2024-05-20 21:07:45 -0600

    configure image for aws

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,11 @@ const nextConfig = {
     serverComponentsExternalPackages: ["@prisma/client", "bcrypt"],
   },
   images: {
-    domains: ['images.unsplash.com', 'images.pexels.com', 'drive.google.com']
+    domains: ['images.unsplash.com', 
+              'images.pexels.com', 
+              'drive.google.com',
+              'direct-upload-s3-bucket-nido.s3.us-east-2.amazonaws.com'
+              ]
   }
 };
 
--- src/app/form/[id]/page.tsx
+++ src/app/form/[id]/page.tsx
@@ -55,7 +55,7 @@ export default function Form() {
 
     }, []); 
 
- 
+    console.log(event);
     const handleSubmit = async (e:any) => {
         const validateEmail = (email:any) => email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
         e.preventDefault();
